7979fec623b393023f69a96baced1d6152714a8f
Conner <hetti031@morris.umn.edu>
2026-01-23 11:36:34 -0600

    Added counter to "Break the bank" card description

--- Goldie/src/main/java/goldieLocks/cards/BreakTheBank.java
+++ Goldie/src/main/java/goldieLocks/cards/BreakTheBank.java
@@ -14,9 +14,6 @@ public class BreakTheBank extends BaseCard{
 
     public static final String ID = makeID(BreakTheBank.class.getSimpleName());
 
-
-
-
     private static final CardStats info = new CardStats(
             MyCharacter.Meta.CARD_COLOR,
             CardType.ATTACK,
@@ -25,8 +22,13 @@ public class BreakTheBank extends BaseCard{
             2
     );
 
+    private static final int DAMAGE = 0;
+    private static final int UPG_DAMAGE = 0;
+
     public BreakTheBank() {
         super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
     }
 
     public void upgrade() {
@@ -38,8 +40,24 @@ public class BreakTheBank extends BaseCard{
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, MyCharacter.GOLD_GAINED_THIS_COMBAT, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
 
+    }
+
+    public void calculateCardDamage(AbstractMonster mo) {
+        int realBaseDamage = this.baseDamage;
+        this.baseDamage = MyCharacter.GOLD_GAINED_THIS_COMBAT;
+        super.calculateCardDamage(mo);
+        this.baseDamage = realBaseDamage;
+        this.isDamageModified = (this.damage != this.baseDamage);
+    }
+
+    public void applyPowers() {
+        int realBaseDamage = this.baseDamage;
+        this.baseDamage += MyCharacter.GOLD_GAINED_THIS_COMBAT;
+        super.applyPowers();
+        this.baseDamage = realBaseDamage;
+        this.isDamageModified = (this.damage != this.baseDamage);
     }
 
     @Override
