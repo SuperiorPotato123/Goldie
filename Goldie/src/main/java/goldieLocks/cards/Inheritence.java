58c5348ad21203dbdb62da001ce865ddaaf1e160
Conner <hetti031@morris.umn.edu>
2026-01-23 00:12:44 -0600

    Add Inheritence

---
+++ Goldie/src/main/java/goldieLocks/cards/Inheritence.java
@@ -0,0 +1,49 @@
+package goldieLocks.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import goldieLocks.character.MyCharacter;
+import goldieLocks.powers.bloodMoneyPower;
+import goldieLocks.powers.inheritencePower;
+import goldieLocks.util.CardStats;
+
+public class Inheritence extends BaseCard{
+
+    public static final String ID = makeID(Inheritence.class.getSimpleName());
+
+
+
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.POWER,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            3
+    );
+
+    public Inheritence() {
+        super(ID, info);
+
+        this.baseMagicNumber = 1;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new inheritencePower(p, magicNumber)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Inheritence();
+    }
+}
---
+++ Goldie/src/main/java/goldieLocks/powers/inheritencePower.java
@@ -0,0 +1,25 @@
+package goldieLocks.powers;
+
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+
+import static goldieLocks.BasicMod.makeID;
+
+public class inheritencePower extends BasePower {
+    public static final String POWER_ID = makeID(inheritencePower.class.getSimpleName());
+    private static final PowerType TYPE = PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+
+
+    public inheritencePower(AbstractCreature owner, int amount) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, amount);
+    }
+
+    public void atStartOfTurn() {
+        this.owner.gainGold(this.amount);
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+}
