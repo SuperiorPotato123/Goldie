faa8ed68e02828899a3f1f00f06965cc30aef61b
Conner <hetti031@morris.umn.edu>
2026-01-28 20:55:16 -0600

    Added Reserve

---
+++ Goldie/src/main/java/goldieLocks/cards/Reserve.java
@@ -0,0 +1,57 @@
+package goldieLocks.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import goldieLocks.character.MyCharacter;
+import goldieLocks.util.CardStats;
+
+public class Reserve extends BaseCard{
+
+    public static final String ID = makeID(Reserve.class.getSimpleName());
+
+
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.COMMON,
+            CardTarget.NONE,
+            -2
+    );
+
+    public Reserve() {
+        super(ID, info);
+    }
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+    }
+
+    @Override
+    public void triggerOnEndOfPlayerTurn() {
+        // Surplus effect
+        if(EnergyPanel.totalCount > 0) {
+            AbstractPlayer p = AbstractDungeon.player;
+            if (!this.upgraded) {
+                addToBot(new ApplyPowerAction(p, p, new EnergizedPower(p, 1), 1));
+            } else {
+                addToBot(new ApplyPowerAction(p, p, new EnergizedPower(p, 2), 2));
+            }
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Reserve();
+    }
+}
