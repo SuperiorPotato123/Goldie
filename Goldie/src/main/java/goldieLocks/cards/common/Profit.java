d3efa2abc0ea79222927b18648b7b223e2fcad35
Conner <hetti031@morris.umn.edu>
2026-02-01 17:51:10 -0600

    Added Proft and changed the description to have unplayable and the surplus keyword. Did the same to other cards that needed those keywords.

---
+++ Goldie/src/main/java/goldieLocks/cards/common/Profit.java
@@ -0,0 +1,66 @@
+package goldieLocks.cards.common;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawCardNextTurnPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import goldieLocks.cards.BaseCard;
+import goldieLocks.character.GainGoldActionExtra;
+import goldieLocks.character.MyCharacter;
+import goldieLocks.util.CardStats;
+
+public class Profit extends BaseCard {
+
+    public static final String ID = makeID(Profit.class.getSimpleName());
+
+
+
+
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.COMMON,
+            CardTarget.NONE,
+            -2
+    );
+
+    public Profit() {
+        super(ID, info);
+
+        this.baseMagicNumber = 3;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(2);
+        }
+    }
+
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+    }
+
+    @Override
+    public void triggerOnEndOfPlayerTurn() {
+        // Surplus effect
+        if(EnergyPanel.totalCount > 0) {
+            AbstractPlayer p = AbstractDungeon.player;
+            addToBot(new GainGoldActionExtra(magicNumber, p));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Profit();
+    }
+}
