ca4e074ea2298e75cd73acf87c3dd8c380eadf06
Conner <hetti031@morris.umn.edu>
2026-02-02 22:06:51 -0600

    Added gold jingle sound when GainGoldActionExtra is used

--- Goldie/src/main/java/goldieLocks/character/GainGoldActionExtra.java
+++ Goldie/src/main/java/goldieLocks/character/GainGoldActionExtra.java
@@ -3,6 +3,7 @@ package goldieLocks.character;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 public class GainGoldActionExtra extends AbstractGameAction{
@@ -17,6 +18,7 @@ public class GainGoldActionExtra extends AbstractGameAction{
 
     public void update() {
         owner.gainGold(this.amount);
+        CardCrawlGame.sound.play("GOLD_JINGLE");
         MyCharacter.GOLD_GAINED_THIS_COMBAT += this.amount;
         this.isDone = true;
     }
