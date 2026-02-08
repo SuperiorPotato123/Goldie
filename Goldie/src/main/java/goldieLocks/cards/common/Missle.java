269197d6ffb6006a92cdaf48c03e565c211da986
Conner <hetti031@morris.umn.edu>
2026-02-08 01:10:15 -0600

    Added Missle

---
+++ Goldie/src/main/java/goldieLocks/cards/common/Missle.java
@@ -0,0 +1,49 @@
+package goldieLocks.cards.common;
+
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import goldieLocks.cards.BaseCard;
+import goldieLocks.character.MyCharacter;
+import goldieLocks.util.CardStats;
+
+public class Missle extends BaseCard {
+
+    public static final String ID = makeID(Missle.class.getSimpleName());
+
+    private static final int DAMAGE = 15;
+    private static final int UPG_DAMAGE = 5;
+
+
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.COMMON,
+            CardTarget.ENEMY,
+            1
+    );
+
+    public Missle() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+        this.baseMagicNumber = 5;
+        this.magicNumber = this.baseMagicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if(p.gold >= magicNumber){
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL)));
+            p.loseGold(magicNumber);
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Missle();
+    }
+}
