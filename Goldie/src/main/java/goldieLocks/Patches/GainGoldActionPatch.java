6d931bb7d283eb02a66a276e8e6ccf8e8e7df374
Conner <hetti031@morris.umn.edu>
2026-02-08 16:24:48 -0600

    Added gold animation for GainGoldAction

--- Goldie/src/main/java/goldieLocks/Patches/GainGoldActionPatch.java
+++ Goldie/src/main/java/goldieLocks/Patches/GainGoldActionPatch.java
@@ -10,6 +10,7 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.EnergyManager;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import com.megacrit.cardcrawl.vfx.GainPennyEffect;
 import goldieLocks.BasicMod;
 import goldieLocks.character.MyCharacter;
 import goldieLocks.powers.LithiumIonPower;
@@ -17,11 +18,14 @@ import goldieLocks.powers.LithiumIonPower;
 public class GainGoldActionPatch {
 
     @SpirePatch2(clz = GainGoldAction.class, method = SpirePatch.CONSTRUCTOR)
-    public static class GoldPatch
-    {
+    public static class GoldPatch {
         public static void Prefix(int amount) {
             MyCharacter.GOLD_GAINED_THIS_COMBAT += amount;
         }
 
+            public static void Postfix(int amount) {
+                for (int i = 0; i < amount; i++)
+                    AbstractDungeon.effectList.add(new GainPennyEffect(100f, 100f));
+            }
+        }
     }
-}
